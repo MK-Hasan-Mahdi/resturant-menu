7be4640a7aef715fd5ebfc21e85e638a5d9e840f
mk-hasan <mkhasan6891@gmail.com>
2022-08-12 01:52:28 +0600

    item filter category wise

--- src/components/MenuData.js
+++ src/components/MenuData.js
@@ -14,10 +14,10 @@ const MenuData = ({ data }) => {
                                             <img src={item.image} alt="Shoes" className="rounded-xl" />
                                         </figure>
                                         <div className="card-body items-center text-center">
-                                            <h2 className="card-title">Shoes!</h2>
-                                            <p>If a dog chews shoes whose shoes does he choose?</p>
+                                            <h2 className="card-title">{item.name}</h2>
+                                            <p>{item.description}</p>
                                             <div className="card-actions">
-                                                <button className="btn btn-primary">Buy Now</button>
+                                                <button className="btn btn-primary">Order Now</button>
                                             </div>
                                         </div>
                                     </div>
--- src/components/Resturant.js
+++ src/components/Resturant.js
@@ -4,9 +4,22 @@ import MenuData from './MenuData';
 
 const Resturant = () => {
     const [menuData, setMenuData] = useState(Menu);
-    console.log(menuData);
+    // console.log(menuData);
+    const filterItem = (category) => {
+        const updatedItemList = Menu.filter((currentElement) => {
+            return currentElement.category === category;
+        });
+        setMenuData(updatedItemList);
+    }
     return (
         <>
+            <div className='flex justify-center mt-10'>
+                <div className="btn-group">
+                    <button onClick={() => filterItem("breakfast")} className="btn btn-active">Breakfast</button>
+                    <button onClick={() => filterItem("lunch")} className="btn">Lunch</button>
+                    <button onClick={() => filterItem("dinner")} className="btn">Dinner</button>
+                </div>
+            </div>
             <MenuData data={menuData} />
         </>
     );
